4abf592d42dffd988dd6dc1724da74c6ced7c549
brunabruniera <brunabruniera2@gmail.com>
2026-07-21 21:17:28 -0300

    primeiro commit

---
+++ sistema.py
@@ -0,0 +1,8 @@
+print("Bem vindo a biblioteca virtual")
+print('''Aqui temos:
+    1 - Cadastro de livros (títulos, autor, código e quantidade);
+    2 - Cadastro de usuários;
+    3 - Empréstimo e devolução de livros;
+    4 - Consulta de livros disponíveis;
+    5 - Relatório de empréstimo.
+''')
